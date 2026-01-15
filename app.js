a986d932351113bbfe1f705c0e1fab73a5545e19
Michele Rochon-Wood <michelerochon@gmail.com>
2026-01-15 18:02:34 -0500

    jan15articlesuggestion

--- app.js
+++ app.js
@@ -510,6 +510,7 @@ app.use('/dashboard', require('./routes/preferenceroutes'));
 app.use('/ui', require('./routes/uiroutes'));
 app.use('/', require('./routes/missionroutes'));
 app.use('/search', require('./routes/searchroutes'));
+app.use('/unitsuggestions', require('./routes/unitsuggestionroutes'));
 
 
 
--- routes/unitsuggestionroutes/index.js
+++ routes/unitsuggestionroutes/index.js
@@ -1,3 +1,8 @@
-const unitSuggestionController = require('../controllers/unitSuggestionController');
+const express = require('express');
+const router = express.Router();
+
+const unitSuggestionController = require('../../controllers/unitSuggestionController');
 
 router.post('/org-suggestions/create', unitSuggestionController.create);
+
+module.exports = router;
