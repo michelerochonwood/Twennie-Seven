670a2f3ff86de6c2d4556d2266b8a4175cfb7d70
Michele Rochon-Wood <michelerochon@gmail.com>
2026-01-19 14:08:00 -0500

    jan19logensurecancontribute

--- middleware/ensureCanContribute.js
+++ middleware/ensureCanContribute.js
@@ -3,6 +3,24 @@
 
 const ALLOWED_MEMBER_LEVELS = new Set(['paid_individual', 'contributor_individual']);
 
+console.log('[ensureCanContribute]', {
+  path: req.originalUrl,
+  hasUser: !!u,
+  userId: u?._id?.toString?.() || u?._id,
+  membershipType: u?.membershipType,
+  hasGroupLeaderEmail: !!u?.groupLeaderEmail,
+  hasEmail: !!u?.email,
+  accessLevel: u?.accessLevel,
+  sessionUser: req.session?.user ? {
+    id: req.session.user.id,
+    membershipType: req.session.user.membershipType,
+    accessLevel: req.session.user.accessLevel,
+    username: req.session.user.username
+  } : null,
+  passportSession: req.session?.passport?.user || null
+});
+
+
 module.exports = function ensureCanContribute(req, res, next) {
   const u = req.user;
   const sessionUser = req.session?.user;
