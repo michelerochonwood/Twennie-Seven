a87c878e17d53d44b48f5d10d7aece0a33fde28f
Michele Rochon-Wood <michelerochon@gmail.com>
2026-01-21 14:55:25 -0500

    jan21ipdtopic

--- controllers/bytopicController.js
+++ controllers/bytopicController.js
@@ -107,7 +107,7 @@ exports.getTopicView = async (req, res) => {
   'Emotional Intelligence': 'emotionalintelligence',
   'Employee Experience': 'employeeexperience',
   'Finding Projects Before they Become RFPs': 'findingprojectsbeforetheybecomerfps',
-  'Integrated Project Delivery or IPD': 'integratedprojectdelivery',
+  'Integrated Project Delivery or IPD': 'integratedprojectdeliveryoripd',
   'Leadership in Technical Consulting': 'leadershipintechnicalconsulting',
   'Leading Change': 'leadingchange',
   'Leading Groups on Twennie': 'leadinggroupsontwennie',
