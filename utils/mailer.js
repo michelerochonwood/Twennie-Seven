603e63860543f68abaf677650eeb9dfb2414e772
Michele Rochon-Wood <michelerochon@gmail.com>
2026-01-28 11:59:09 -0500

    jan28utilsmailer

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,22 +1,14 @@
 const nodemailer = require('nodemailer');
 
-function createTransport() {
-  // Recommended vars:
-  // SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS, SMTP_FROM
-  return nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT || 587),
-    secure: Number(process.env.SMTP_PORT) === 465,
-    auth: process.env.SMTP_USER
-      ? { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS }
-      : undefined
-  });
-}
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT || 587),
+  secure: false, // STARTTLS
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
-async function sendMail({ to, subject, html, text }) {
-  const transporter = createTransport();
-  const from = process.env.SMTP_FROM || 'Twennie <info@twennie.com>';
-  return transporter.sendMail({ from, to, subject, html, text });
-}
+module.exports = transporter;
 
-module.exports = { sendMail };
