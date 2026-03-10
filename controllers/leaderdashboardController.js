831a2b24a2c57de1297fdd031c4b2868b3743b11
Michele Rochon-Wood <michelerochon@gmail.com>
2026-03-10 12:52:48 -0400

    mar9upcomingunits

--- controllers/leaderdashboardController.js
+++ controllers/leaderdashboardController.js
@@ -1191,25 +1191,26 @@ const [leaderUpcomings, leaderNuggets] = await Promise.all([
 ]);
 
 let leaderUnits = await Promise.all(
-  [...leaderArticles, ...leaderVideos, ...leaderPromptSets, ...leaderInterviews, ...leaderExercises, ...leaderTemplates, ...leaderUpcomings, ...leaderNuggets, ...leaderMissions].map(async (unit) => {
+  [...leaderArticles, ...leaderVideos, ...leaderPromptSets, ...leaderInterviews, ...leaderExercises, ...leaderTemplates, ...leaderNuggets, ...leaderMissions].map(async (unit) => {
     const author = await resolveAuthorById(pickAuthorId(unit));
-return {
-  unitType: normalizeUnitType(unit),
-  title:
-    unit.mission_title ||
-    unit.article_title ||
-    unit.video_title ||
-    unit.promptset_title ||
-    unit.interview_title ||
-    unit.exercise_title ||
-    unit.template_title ||
-    unit.title ||
-    "Untitled Unit",
-  status: unit.status || "Unknown",
-  mainTopic: unit.main_topic || unit.discipline || unit.client || unit.region || "No topic",
-  _id: unit._id,
-  author: author.name
-};
+
+    return {
+      unitType: normalizeUnitType(unit),
+      title:
+        unit.mission_title ||
+        unit.article_title ||
+        unit.video_title ||
+        unit.promptset_title ||
+        unit.interview_title ||
+        unit.exercise_title ||
+        unit.template_title ||
+        unit.title ||
+        "Untitled Unit",
+      status: unit.status || "Unknown",
+      mainTopic: unit.main_topic || unit.discipline || unit.client || unit.region || "No topic",
+      _id: unit._id,
+      author: author.name
+    };
   })
 );
 
