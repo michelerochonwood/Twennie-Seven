7814611e72247792aff588ad4fbeb63ec8f37756
Michele Rochon-Wood <michelerochon@gmail.com>
2026-05-06 16:38:58 -0400

    may6billlings

--- controllers/billingController.js
+++ controllers/billingController.js
@@ -2,7 +2,7 @@
 const Stripe = require('stripe');
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2023-10-16' });
 const Leader = require('../models/member_models/leader');
-const { syncLeaderSeatQuantity } = require('../billing/stripeSeats');
+const { syncLeaderSeatQuantity } = require('../billings/stripeSeats');
 
 function requireLeader(req, res, next) {
   // You said you use req.user (Passport). Adjust if different.
