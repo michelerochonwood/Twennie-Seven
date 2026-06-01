4850e9414f7754f618e6a68b85b3a7dfd951e405
Michele Rochon-Wood <michelerochon@gmail.com>
2026-06-01 16:30:15 -0400

    jun1controller

--- routes/demoroutes/index.js
+++ routes/demoroutes/index.js
@@ -1,7 +1,7 @@
 // routes/demoRoutes.js
 const express = require('express');
 const router = express.Router();
-const demoController = require('../controllers/demoController');
+const demoController = require('../../controllers/demoController');
 
 router.get('/book-a-demo', demoController.showDemoForm);
 router.post('/book-a-demo', demoController.submitDemoRequest);
