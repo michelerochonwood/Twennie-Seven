261c074cfafec56028f05203ca8e3f3011f0cfd0
Michele Rochon-Wood <michelerochon@gmail.com>
2026-06-02 15:00:09 -0400

    june2demo

--- controllers/demoController.js
+++ controllers/demoController.js
@@ -45,3 +45,5 @@ exports.submitDemoRequest = async (req, res) => {
     });
   }
 };
+
+
--- routes/demoroutes/index.js
+++ routes/demoroutes/index.js
@@ -1,15 +1,15 @@
 // routes/demoRoutes.js
 const express = require('express');
 const router = express.Router();
-const demoController = require('../../controllers/demoController');
+const demoController = require('../controllers/demoController');
 
 router.get('/book-a-demo', demoController.showDemoForm);
 router.post('/book-a-demo', demoController.submitDemoRequest);
 
 router.get('/book-a-demo/success', (req, res) => {
-  res.render('promo_views/demo_success', {
-    layout: 'mainlayout'
-  });
+res.render('promo_views/demo_success', {
+layout: 'mainlayout'
+});
 });
 
 module.exports = router;
